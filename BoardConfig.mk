8bf5bd265841f771adc8a410605ded3b066e7a41
demetris <ierokipides.dem@gmail.com>
2014-05-27 23:30:46 +0300

    Try new toolchains from Chet: GCC_ARM (kernel): Linaro 4.8.3 GCC_AND (ROM): Sabermod 4.8 Credit:CPA-Poke

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,9 @@ TARGET_RECOVERY_PRE_COMMAND := "/system/bin/setup-recovery"
 
 # Try to build the kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_x3_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.7
+TARGET_GCC_VERSION_ARM := LK
+TARGET_GCC_VERSION_AND := 4.8-sm
+
 # Keep this as a fallback
 TARGET_PREBUILT_KERNEL := device/lge/p880/kernel
 
